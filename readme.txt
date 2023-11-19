88e840ea335e110bf607f8c8360ebf926fc3b8d8
Yanzheng(Dexter) Wu <ywu1188@163.com>
2023-11-18 22:07:48 -0500

    final version

--- readme.txt
+++ readme.txt
@@ -4,3 +4,12 @@ Three main pros of using a rate limiter for your API system:
 1. Prevent resource starvation caused by simple DOS attack(DDOS not covered).
 2. Reduce running server costs.
 3. Prevent overloading the servers from excess requests.
+
+Instruction on how to run the files.
+This repo contains two main files: the server file and the database file.
+To start the server, you need to have Redis installed and start the redis server
+manually.
+Upon providing a .env file with your credentials, the server will connect to the 
+database server, which we can test the two implemented rate limiters through:
+curl -H 'user: name1' http://localhost:3000/current_user_simple
+curl -H 'user: name1' http://localhost:3000/current_user_redis
--- server.js
+++ server.js
@@ -1,31 +1,37 @@
 //This is a mocking server testing the rate limiter.
 const { access } = require('fs');
 const db = require('./DB');
+const redis = require('redis');
+const client = redis.createClient({url: 'redis://localhost:6379'});//Setting Redis on localhost
 const express = require('express');
 const app = express();
 
+client.on('error', err => console.log('Redis client error', err));
+client.connect();
 app.listen(3000, () => console.log('Connected to server.'));
 
 //Shutting down db server properly
 //Signal interrupt: Triggers when user manually interrupt a process
-process.on('SIGINT', () => {
+process.on('SIGINT', async () => {
     console.log('Server is shutting down');
-    db.closePool(); // Close the database pool
+    await client.quit();
+    db.closePool();
     process.exit(0);
 });
 //Signal terminate: Triggers when processes request its termination
-process.on('SIGTERM', () => {
+process.on('SIGTERM', async () => {
     console.log('Server is shutting down');
-    db.closePool(); // Close the database pool
+    await client.quit();
+    db.closePool();
     process.exit(0);
 });
 
 const req_table = {};
 
 //For testing purpose, assuming the incoming request has a format like this:
-//curl -H 'user: name1' http://localhost:3000/current_user
+//curl -H 'user: name1' http://localhost:3000/current_user_methods
 //Need to fix db's get function if want to support more psql queries.
-app.get('/current_user', function(req, res) {
+app.get('/current_user_simple', function(req, res) {
     //Rate Limiter, If same user tries submit two requests in between 5s
     //access will get denied due to rate limiting. 
     const {user} = req.headers;
@@ -42,7 +48,36 @@ app.get('/current_user', function(req, res) {
             res.send(err + '\n');
         } else {
             req_table[user] = Date.now();
+            //console.log(typeof info);
             res.send(info + '\n');
         }
     });
+});
+
+app.get('/current_user_redis', async function(req, res) {
+    //Rate Limiter, Same 5s but through Redis module. 
+    const {user} = req.headers;
+    try {
+        const last_req = await client.get(user);
+
+        if (last_req) {
+            if (Date.now() - last_req < 5000) {
+                res.status(429).send('Access Denied. Too many requets.');
+                return;
+            }
+        }
+
+        db.get('current_user', (err, info) => {
+            if (err) {
+                res.send(err + '\n');
+            } else {
+                client.set(user, Date.now());
+                //console.log(typeof info);
+                res.send(info + '\n');
+            }
+        });
+    } catch (err) {
+        console.error('Redis error: ', err);
+        res.status(500).send('Internal Server Error')
+    }
 });
